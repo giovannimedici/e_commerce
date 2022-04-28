ad4c91c2aed661e2a24fadc9651dd8059d39829a
giovannimedici <giovannigilsu@gmail.com>
2022-04-28 08:10:29 -0300

    Checkout - PaymentForm #1

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -7,7 +7,7 @@ import {commerce } from '../../lib/commerce';
 
 import FormInput from './CustomTextField';
 
-const AddressForm = ({checkoutToken}) => {
+const AddressForm = ({checkoutToken, next}) => {
   const [shippingCountries, setShippingCountries] = useState([]);
   const [shippingCountry, setShippingCountry] = useState('');
   const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
@@ -57,7 +57,7 @@ const AddressForm = ({checkoutToken}) => {
     <>
       <Typography variant="h6" gutterBottom>Shipping Address</Typography>
       <FormProvider {... methods}>
-        <form onSubmit=''>
+        <form onSubmit={methods.handleSubmit((data) => next({... data, shippingCountry, shippingSubdivision, shippingOption}))}>
           <Grid container spacing={3}>
             <FormInput name='firstName' label='First name'/>
             <FormInput name='lastName' label='Last name'/>
@@ -99,7 +99,7 @@ const AddressForm = ({checkoutToken}) => {
           <br/>
           <div style={{display: 'flex', justifyContent: 'space-between'}}>
             <Button component={Link} to="/cart" variant="outlined">Back to Cart</Button>
-            <Button type="submit" variant="contained" color="primary" disabled="disabled">Next</Button>
+            <Button type="submit" variant="contained" color="primary"k>Next</Button>
           </div>
         </form>
       </FormProvider>
